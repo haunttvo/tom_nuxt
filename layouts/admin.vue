12ff5728f2e9a9ec240f51b5acbb214ea8d80013
haunttvo <haunt@tinhvan.com>
2019-03-04 18:02:47 +0700

    tom nuxt

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        $('.menu-sidebar-items a.ls_down').click(function(){
+            if( $(this).parent().hasClass('active') ){
+                $(this).parent().removeClass('active');
+                $(this).next('.sidebar-submenu').slideUp(200);
+            }else{
+                console.log(1);
+                $('.menu-sidebar-items').removeClass('active');
+                $(this).parent().addClass('active');
+                $(".menu-sidebar-items .sidebar-submenu").slideUp(200);
+                $(this).next('.sidebar-submenu').slideDown(200);
+            }
+        });
+    }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet" , type: "text/css" , href:'https://use.fontawesome.com/releases/v5.7.2/css/all.css' }
+    ],
+    script:[
+      {src:'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'},
+      {src:'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'}
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    '@/assets/admin/css/main.scss'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
